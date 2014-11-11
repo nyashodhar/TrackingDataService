2401a9a13cd202d786186fd7a5e0e23c9db60d0c
Per Blix <per.blix@mac.com>
2014-11-10 21:03:30 -0800

    Move query result adjustment code into bucket aggregration facade

--- src/main/java/com/petpal/tracking/service/TrackingDataService.java
+++ src/main/java/com/petpal/tracking/service/TrackingDataService.java
@@ -3,7 +3,6 @@ package com.petpal.tracking.service;
 import com.petpal.tracking.data.TrackingData;
 import com.petpal.tracking.service.util.QueryArgumentValidationUtil;
 import com.petpal.tracking.service.util.QueryLoggingUtil;
-import org.apache.commons.lang.math.LongRange;
 import org.apache.log4j.Logger;
 import org.kairosdb.client.KairosClientUtil;
 import org.kairosdb.client.KairosRestClient;
@@ -18,20 +17,17 @@ import org.kairosdb.client.response.Queries;
 import org.kairosdb.client.response.QueryResponse;
 import org.kairosdb.client.response.Response;
 import org.kairosdb.client.response.Results;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TimeZone;
 import java.util.TreeMap;
 
 /**
@@ -41,6 +37,9 @@ import java.util.TreeMap;
 public class TrackingDataService {
 
     private Logger logger = Logger.getLogger(this.getClass());
+
+    @Autowired
+    private BucketAggregationUtil bucketAggregationUtil;
 
     @Value("${trackingService.kairosDBHost}")
     private String kairosDBHost;
@@ -213,9 +212,12 @@ public class TrackingDataService {
             //
             // Adjust the bucket boundaries and inject empty buckets.
             //
-            Map<Long, Long> adjustedMetricResult = adjustBoundariesForMetricResult(
-                    trackingMetric, metricResults.get(trackingMetric), utcBegin, utcEnd, resultBucketSize);
+            Map<Long, Long> adjustedMetricResult = bucketAggregationUtil.adjustBoundariesForMetricResult(
+                    metricResults.get(trackingMetric), utcBegin, utcEnd, resultBucketSize);
             metricResults.put(trackingMetric, adjustedMetricResult);
+
+            logger.debug("Bucket adjust for metric " + trackingMetric + ": old bucket count = " +
+                    metricResults.get(trackingMetric).size() + ", new bucket count = " + adjustedMetricResult.size());
 
             // If the response is not to be verbose, filter out the empty buckets.
             if(!verboseResponse) {
@@ -227,145 +229,6 @@ public class TrackingDataService {
                 }
             }
         }
-    }
-
-
-    //
-    // When aggregating the output into buckets, the timestamp of the bucket is unfortunately
-    // NOT equal to the start of the bucket interval (which would have been logical). Rather
-    // the timestamp of the sum in a bucket is equal to the first timestamp for which a match
-    // was found in the bucket interval.
-    //
-    // This method adjusts the bucket starts of each bucket.
-    //
-    // For bucket intervals for which no data was found, the result does not include a bucket.
-    // This method will insert an empty bucket tied to the start of the interval for which
-    // no data was found.
-    //
-    // Example:
-    //    Database has datapoints May 29 - 3 steps, July 2nd 2 steps
-    // Query:
-    //    Get data starting May 1st - Aug 1
-    // Result
-    //    Bucket 1 - timestamp May 29 - value/sum = 3
-    //    Bucket 2 - timestamp July 2nd - value/sum = 3
-    // After transform:
-    //    Bucket 1 - timestamp May 1 - value/sum = 3
-    //    Bucket 2 - timestamp June 1 - value/sum = 0
-    //    Bucket 2 - timestamp July 1 - value/sum = 3
-    //
-    private Map<Long, Long> adjustBoundariesForMetricResult(
-            TrackingMetric trackingMetric, Map<Long, Long> metricResult, Long utcBegin, Long utcEnd, TimeUnit resultBucketSize) {
-
-        if(utcBegin == null) {
-            throw new IllegalArgumentException("utcBegin not specified");
-        }
-
-        if(utcEnd != null && (utcEnd.longValue() <= utcBegin.longValue())) {
-            throw new IllegalArgumentException("The utcEnd value ("+utcEnd+") must be greater than utcBegin value (" + utcBegin + ")");
-        }
-
-        Calendar cursorCalendar = new GregorianCalendar();
-        cursorCalendar.setTimeZone(TimeZone.getTimeZone("UTC"));
-        cursorCalendar.setTimeInMillis(utcBegin);
-
-        long endOfInterval = (utcEnd == null) ? System.currentTimeMillis() : utcEnd;
-
-        //
-        // Generate all the correct bucket boundaries
-        // This will also ensure that any missing empty buckets are created.
-        //
-
-        Map<Long, Long> newMetricResult = new TreeMap<Long, Long>();
-
-        while(true) {
-            newMetricResult.put(cursorCalendar.getTimeInMillis(), 0L);
-
-            //
-            // Step one bucket size unit forward. If we're beyond
-            // the end of the interval don't add any more buckets.
-            //
-            int calenderUnitToStepForward = getCalendarUnitForTimeUnit(resultBucketSize);
-            cursorCalendar.add(calenderUnitToStepForward, 1);
-            if(cursorCalendar.getTimeInMillis() >= endOfInterval) {
-                break;
-            }
-        }
-
-        logTimeStampAdjustments(trackingMetric, metricResult, newMetricResult);
-
-        //
-        // Walk through all the previous results, and drop them into the correct corresponding bucket
-        // TODO: Since it's a treemap, we can do this in linear time but for now just brute attack...
-        //
-
-        List<Long> newBucketTimeStamps = new ArrayList<Long>(newMetricResult.size());
-        newBucketTimeStamps.addAll(newMetricResult.keySet());
-
-        List<LongRange> newBucketRanges = new ArrayList<LongRange>(newMetricResult.size());
-
-        for (int i = 0; i < newBucketTimeStamps.size(); i++) {
-            long timeStampAtIndex = newBucketTimeStamps.get(i);
-            if(i == (newBucketTimeStamps.size()-1)) {
-                newBucketRanges.add(new LongRange(timeStampAtIndex, endOfInterval));
-            } else {
-                newBucketRanges.add(new LongRange(timeStampAtIndex, newBucketTimeStamps.get(i+1)-1));
-            }
-        }
-
-        for(long oldTimeStamp : metricResult.keySet()) {
-            for (LongRange newBucketRange : newBucketRanges) {
-                if (newBucketRange.containsLong(oldTimeStamp)) {
-                    newMetricResult.put(newBucketRange.getMinimumLong(), metricResult.get(oldTimeStamp));
-                }
-            }
-        }
-
-        return newMetricResult;
-    }
-
-    private int getCalendarUnitForTimeUnit(TimeUnit timeUnit) {
-
-        if(timeUnit == TimeUnit.YEARS) {
-            return Calendar.YEAR;
-        } else if(timeUnit == TimeUnit.MONTHS) {
-            return Calendar.MONTH;
-        } else if(timeUnit == TimeUnit.WEEKS) {
-            return Calendar.WEEK_OF_YEAR;
-        } else if(timeUnit == TimeUnit.DAYS) {
-            return Calendar.DATE;
-        } else if(timeUnit == TimeUnit.HOURS) {
-            return Calendar.HOUR;
-        } else if(timeUnit == TimeUnit.MINUTES) {
-            return Calendar.MINUTE;
-        } else {
-            throw new IllegalArgumentException("Unexpected TimeUnit " + timeUnit);
-        }
-    }
-
-    private void logTimeStampAdjustments(TrackingMetric trackingMetric, Map<Long, Long> metricResult, Map<Long, Long> newMetricResult) {
-
-        logger.debug("Bucket adjust for metric " + trackingMetric + ": old bucket count = " + metricResult.size() + ", new bucket count = " + newMetricResult.size());
-
-        StringBuilder oldTimeStamps = new StringBuilder();
-        for(long oldTimeStamp : metricResult.keySet()) {
-            if(oldTimeStamps.length() > 0) {
-                oldTimeStamps.append(", ");
-            }
-            oldTimeStamps.append(QueryLoggingUtil.getUTCFormat(oldTimeStamp) + "(" + oldTimeStamp  + ")");
-        }
-
-        logger.debug("Old bucket boundaries: [" + oldTimeStamps + "]");
-
-        StringBuilder newTimeStamps = new StringBuilder();
-        for(long newTimeStamp : newMetricResult.keySet()) {
-            if(newTimeStamps.length() > 0) {
-                newTimeStamps.append(", ");
-            }
-            newTimeStamps.append(QueryLoggingUtil.getUTCFormat(newTimeStamp) + "(" + newTimeStamp  + ")");
-        }
-
-        logger.debug("New bucket boundaries: [" + newTimeStamps + "]");
     }
 
 
@@ -389,6 +252,7 @@ public class TrackingDataService {
         logger.info("Metric " + trackingMetric + " prepped: " + timeStampValueMap.size() + " data points, tags = " + tags);
     }
 
+    
     @PostConstruct
     public void afterPropertiesSet() throws Exception {
         String kairosDBURL = "http://" + kairosDBHost + ":" + kairosDBPort;
