c99e87225683de9dd9adabc96332b0260832b6f2
Per Blix <per.blix@mac.com>
2014-11-12 12:26:11 -0800

    Introduce enum for aggregated metrics and method to convert between enums using bucket size

---
+++ src/main/java/com/petpal/tracking/service/AggregatedTrackingMetric.java
@@ -0,0 +1,9 @@
+package com.petpal.tracking.service;
+
+/**
+ * This enumeration names metrics for all the actual time series that are
+ * stored to back the types of metrics an end user can refer
+ * Created by per on 11/12/14.
+ */
+public class AggregatedTrackingMetric {
+}
---
+++ src/test/java/com/petpal/tracking/service/AggregatedTrackingMetricTest.java
@@ -0,0 +1,7 @@
+package com.petpal.tracking.service;
+
+/**
+ * Created by per on 11/12/14.
+ */
+public class AggregatedTrackingMetricTest {
+}
