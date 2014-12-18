6a56d1f1c760f3e840aacbbc78c6c042969506f8
Per Blix <per.blix@mac.com>
2014-12-17 20:37:00 -0800

    Encapsulate code used roll new injected values into existing datapoints

---
+++ src/main/java/com/petpal/tracking/service/DataPointAggregationUtil.java
@@ -0,0 +1,7 @@
+package com.petpal.tracking.service;
+
+/**
+ * Created by per on 12/17/14.
+ */
+public class DataPointAggregationUtil {
+}
---
+++ src/test/java/com/petpal/tracking/service/DataPointAggregationUtilTest.java
@@ -0,0 +1,8 @@
+package com.petpal.tracking.service;
+
+/**
+ * Created by per on 12/17/14.
+ */
+public class DataPointAggregationUtilTest {
+
+}
